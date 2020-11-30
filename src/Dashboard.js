ea866d4c36304a18c5c87f03f21d389a83385b06
Saumitra Paul <skpaul@gmail.com>
2020-11-30 19:16:21 +0600

    kjhjk

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,27 +3,20 @@ import Axios from "axios";
 
 export default function Dashboard(props){
    let eiin = localStorage.getItem('eiin');
-   
-   // remove
-    //localStorage.removeItem('myData');
 
-    // remove all
-    // localStorage.clear();
-
-   console.log(eiin);
    if(eiin == null || eiin === ""){
-       props.history.push('/'); // Redirect to dashboard
+       props.history.push('/');
    }
 
    const [district, setDistrict] = useState("");
    useEffect(()=>{
-        console.log("use effect");
+        // console.log("use effect");
 
         let postData = new FormData();
         postData.append("eiin", eiin);
         Axios.post("http://localhost/seat-plan/api/institute-details.php",postData).then(response=>{
             let data = response.data;
-            console.log(data.level);
+            // console.log(data.level);
             setDistrict(data.district);
         }).catch(error=>{
             console.log(error);
@@ -32,24 +25,51 @@ export default function Dashboard(props){
    },[]);
 
    useEffect(()=>{
-    console.log("use effect");
+    // console.log("use effect");
 
     let postData = new FormData();
     postData.append("eiin", eiin);
     Axios.post("http://localhost/seat-plan/api/institute-details.php",postData).then(response=>{
         let data = response.data;
-        console.log(data.level);
+        // console.log(data.level);
     }).catch(error=>{
         console.log(error);
     });
 
 },[]);
 
+const [divs, setDiv] = useState([]);
+const [counterKey, setCounterKey] = React.useReducer(c => c + 1, 0);
+
+const [count, setCount] = React.useState(0);
+  
+
+ const addNewDiv = event=>{
+     event.preventDefault();
+     setCount(count => count + 1);
+     console.log(count);
+     setDiv(divs => [...divs, <Testdiv key={count} val={count}/>]);
+ }
+
+    const allDivs = divs.map( item => (
+        item
+    ))
+
     return(
         <>
             <div>Im Dashboard {district}</div>
             <div>{eiin}</div>
+            <button onClick={addNewDiv}>Add Div</button>
+            <div>
+
+            {allDivs}
+            </div>
         </>
     ) ;
 
+}
+
+ function Testdiv(props){
+    console.log(props.val);
+    return (<h1 key={props.val}>HI</h1>);
 }
