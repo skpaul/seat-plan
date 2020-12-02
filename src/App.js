24a29a4e2452c15fabcdbe9ed8fba144242b86f1
Saumitra Paul <skpaul@gmail.com>
2020-12-03 00:20:18 +0600

    top nav added.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import ViewSeatPlan from "./components/ViewSeatPlan";
+import TopNav from "./components/TopNav";
 
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     
+    
       <Router>
+        <TopNav/>
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/dashboard" component={Dashboard} />
---
+++ src/components/TopNav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import "./TopNav.css";
+export default function TopNav(){
+    return(
+        <div className="top-nav">
+            <div>
+                <Link to="/dashboard">Profile</Link>
+            </div>
+            <div>
+
+            <Link to="/create-seat-plan">Create Seat Plan</Link>
+            </div>
+            <div>
+
+            <Link to="/view-seat-plan">View Seat Plan</Link>
+            </div>
+
+        </div>
+    );
+}
